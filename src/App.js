cce7fe6a0f6a5b38a789e8bf849f98d3764e42c6
GStormx2 <tanqir.msarker@gmail.com>
2020-10-25 09:59:19 +0600

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      pokemon: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    let temp = []
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+      .then(response => response.json())
+      .then(allPokemon => {
+        allPokemon.results.forEach(p => {
+          fetch(p.url)
+            .then(res => res.json())
+            .then(poke => {
+              let arr = [];
+              let test = poke.stats;
+              let hp = test[0].base_stat;
+              let atk = test[1].base_stat;
+              let def = test[2].base_stat;
+              let test2 = poke.types;
+              for(let i=0; i<test2.length; i++){
+                  arr.push(test2[i].type.name);
+              }
+              temp.push({
+                  id: poke.id,
+                  name: poke.name,
+                  health: hp,
+                  attack: atk,
+                  defense: def,
+                  types: arr 
+              })
+            })
+        })
+      });
+      this.setState({pokemon: temp});
+  }  
+  render() {
+    return (
+      <div className='App'>
+        <CardList pokemon={this.state.pokemon}/>
+      </div>
+    );
+  }
 }
 
 export default App;
